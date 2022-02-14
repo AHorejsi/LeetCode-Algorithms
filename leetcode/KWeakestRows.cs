93da23bc64f38211ea4a0a669acad0415b480aa5
Alex Horejsi <alex.horejsi59@gmail.com>
2022-02-14 15:07:34 -0800

    Update KWeakestRows.cs

--- leetcode/KWeakestRows.cs
+++ leetcode/KWeakestRows.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public int[] KWeakestRows(int[][] mat, int numberOfWeakest) => mat
-        .Select((int[] row, int index) => (row.Sum(), index))
-        .OrderBy(((int strength, int index) val) => val.strength)
-        .Take(numberOfWeakest)
-        .Select(((int strength, int index) val) => val.index)
-        .ToArray();
+        .Select((int[] row, int index) => (row.Sum(), index)) // Since the matrix is a binary matrix and the amount of ones indicates the rows strength, the sum of a row will represent the strength of a row with a single number
+        .OrderBy(((int strength, int index) val) => val.strength) // Sort based on previously computed strengths
+        .Take(numberOfWeakest) // Take amount of entries needed
+        .Select(((int strength, int index) val) => val.index) // Pull out the indices of the strongest rows
+        .ToArray(); // Convert to array
 }
