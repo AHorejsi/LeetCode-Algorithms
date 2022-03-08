9902e4625964b5bc3442c43d4fde0ae79ab7f0e2
Alex Horejsi <alex.horejsi59@gmail.com>
2022-03-08 08:45:38 -0800

    Update BalancedBinaryTree.cs

--- leetcode/BalancedBinaryTree.cs
+++ leetcode/BalancedBinaryTree.cs
@@ -12,40 +12,66 @@
  * }
  */
 public class Solution {
-    public bool IsBalanced(TreeNode root) => root is null || -1 != this.TreeHeight(root, 0);
+    // This value will be used  to indicate that a given subtree is imbalanced
+    private const int ImbalancedHeight = -1;
+    
+    public bool IsBalanced(TreeNode root) => root is null || Solution.ImbalancedHeight != this.TreeHeight(root, 0);
     
     private int TreeHeight(TreeNode node, int height) {
-        int leftHeight = height;
-        int rightHeight = height;
-        
-        if (!(node.left is null)) {
-            leftHeight = this.TreeHeight(node.left, height);
-            
-            if (-1 == leftHeight) {
-                return leftHeight;
-            }
-            else {
-                ++leftHeight;
-            }
-        }
-        if (!(node.right is null)) {
-            rightHeight = this.TreeHeight(node.right, height);
-            
-            if (-1 == rightHeight) {
-                return rightHeight;
-            }
-            else {
-                ++rightHeight;
-            }
+        if (node is null) {
+            // If the "node" is null, then the current subtree cannot be
+            // traversed further. The max height of this subtree has been found
+            return height;
         }
         
+        // The height of the left subtree
+        int leftHeight = this.TreeHeight(node.left, height);
+        
+        if (Solution.ImbalancedHeight == leftHeight) {
+            // If a previous subtree was found to be imbalanced,
+            // then return the value that indicates the whole tree
+            // is imbalanced
+            return Solution.ImbalancedHeight;
+        }
+        else {
+            // Otherwise, increment "leftHeight" because the height returned
+            // by the above, recursive call is the height of the left subtree
+            // of "node". So the height of the current subtree is one greater
+            ++leftHeight;
+        }
+        
+        // The height of the right subtree
+        int rightHeight = this.TreeHeight(node.right, height);
+        
+        if (Solution.ImbalancedHeight == rightHeight) {
+            // If a previous subtree was found to be imbalanced,
+            // then return the value that indicates the whole tree
+            // is imbalanced
+            
+            return Solution.ImbalancedHeight;
+        }
+        else {
+            // Otherwise, increment "rightHeight" because the height returned
+            // by the above, recursive call is the height of the right subtree
+            // of "node". So the height of the current subtree is one greater
+            
+            ++rightHeight;
+        }
+        
+        // The difference in heights between the two subtrees of "node"
         int heightDifference = Math.Abs(leftHeight - rightHeight);
-
         if (heightDifference <= 1) {
+            // If the height of the two subtrees differ by 0 or 1,
+            // then this subtree is balanced. The height of this
+            // subtree is considered to be the greater of the two heights
+            
             return Math.Max(leftHeight, rightHeight);
         }
         else {
-            return -1;
+            // If the subtrees have a height difference greater than one,
+            // then the whole tree is imbalanced
+            
+            return Solution.ImbalancedHeight;
         }
     }
 }
