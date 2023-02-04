af879b0f93a91edb8044ed9e99116c5b968b346d
Alex Horejsi <alex.horejsi59@gmail.com>
2023-02-04 13:10:48 -0800

    Update BalancedBinaryTree.cs

--- leetcode/BalancedBinaryTree.cs
+++ leetcode/BalancedBinaryTree.cs
@@ -15,63 +15,36 @@ public class Solution {
     // Used  to indicate that a given subtree is imbalanced
     private const int ImbalancedHeight = -1;
     
-    public bool IsBalanced(TreeNode root) => root is null || Solution.ImbalancedHeight != this.TreeHeight(root, 0);
+    // Entry point
+    public bool IsBalanced(TreeNode root) => Solution.ImbalancedHeight != this.TreeHeight(root, 0);
     
+    // Computes the height of a given node of a binary tree
     private int TreeHeight(TreeNode node, int height) {
         if (node is null) {
             // If the "node" is null, then the current subtree cannot be
             // traversed further. The max height of this subtree has been found
             return height;
         }
-        
-        // The height of the left subtree
-        int leftHeight = this.TreeHeight(node.left, height);
-        
-        if (Solution.ImbalancedHeight == leftHeight) {
-            // If a previous subtree was found to be imbalanced,
-            // then return the value that indicates the whole tree
-            // is imbalanced
-            return Solution.ImbalancedHeight;
-        }
-        else {
-            // Otherwise, increment "leftHeight" because the height returned
-            // by the above, recursive call is the height of the left subtree
-            // of "node". So the height of the current subtree is one greater
-            ++leftHeight;
-        }
-        
-        // The height of the right subtree
-        int rightHeight = this.TreeHeight(node.right, height);
-        
-        if (Solution.ImbalancedHeight == rightHeight) {
-            // If a previous subtree was found to be imbalanced,
-            // then return the value that indicates the whole tree
-            // is imbalanced
-            
-            return Solution.ImbalancedHeight;
-        }
-        else {
-            // Otherwise, increment "rightHeight" because the height returned
-            // by the above, recursive call is the height of the right subtree
-            // of "node". So the height of the current subtree is one greater
-            
-            ++rightHeight;
-        }
+
+        // Height of the left subtree
+        var leftHeight = this.TreeHeightHelper(node.left, height);
+
+        // Height of the right subtree
+        var rightHeight = this.TreeHeightHelper(node.right, height);
         
         // The difference in heights between the two subtrees of "node"
-        int heightDifference = Math.Abs(leftHeight - rightHeight);
-        if (heightDifference <= 1) {
-            // If the height of the two subtrees differ by 0 or 1,
-            // then this subtree is balanced. The height of this
-            // subtree is considered to be the greater of the two heights
-            
-            return Math.Max(leftHeight, rightHeight);
-        }
-        else {
-            // If the subtrees have a height difference greater than one,
-            // then the whole tree is imbalanced
-            
-            return Solution.ImbalancedHeight;
-        }
+        var heightDifference = Math.Abs(leftHeight - rightHeight);
+
+        // If the height difference is greater than 1, then the tree is, by the definition provided by the problem, imbalanced.
+        // Otherwise, we use the taller of the two subtrees
+        return heightDifference > 1 ? Solution.ImbalancedHeight : Math.Max(leftHeight, rightHeight);
+    }
+
+    private int TreeHeightHelper(TreeNode childNode, int height) {
+        // Height of the current subtree represented by the child node
+        var childHeight = this.TreeHeight(childNode, height);
+
+        // If the subtree represented by the child node is balanced, then return the height of the child node's parent
+        return Solution.ImbalancedHeight == childHeight ? Solution.ImbalancedHeight : childHeight + 1;
     }
 }
