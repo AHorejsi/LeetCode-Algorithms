efbb12733ba0a1a30e548d34735552ba3d3f93eb
Alex Horejsi <alex.horejsi59@gmail.com>
2023-07-04 15:48:42 -0700

    Update NQueensSolutionCount.cs

--- leetcode/NQueensSolutionCount.cs
+++ leetcode/NQueensSolutionCount.cs
@@ -1,36 +1,25 @@
 public class Solution {
-    public int TotalNQueens(int dimensions) => this.BuildTable(, 0, dimensions);
+    public int TotalNQueens(int dimensions) {
+        SafetyTable safety = new SafetyTable(dimensions);
+        int rowIndex = 0;
+        
+        return this.BuildTable(safety, rowIndex, dimensions);
+    }
     
     private int BuildTable(SafetyTable safety, int rowIndex, int dimensions) {
         if (dimensions == rowIndex) {
-            // If the current row index is equal to the dimensions of the chess board,
-            // then a solution has been found and there is only one solution corresponding
-            // to the current board state
-            
             return 1;
         }
         
-        // Tracks the number of solutions found during any recursive calls
         int solvedCount = 0;
-        
-        // The index of the following row
         int nextRowIndex = rowIndex + 1;
-        
-        // Traverse the columns of the current row for safe spaces
+
         for (int colIndex = 0; colIndex < dimensions; ++colIndex) {
             if (safety.IsSafe(rowIndex, colIndex)) {
-                // If the position corresponding to "rowIndex" and "colIndex" is safe,
-                // a queen can be placed there
-                
-                // Toggle safety of the current space. Since the board was initialized
-                // to all spaces being safe, this call will only set the current space to unsafe
                 safety.ToggleSafety(rowIndex, colIndex);
-                
-                // Add any solutions found in the foloowing recursive call to the total count
+
                 solvedCount += this.BuildTable(safety, nextRowIndex, dimensions);
-                
-                // Toggle the safety of the current space. Since the given space was 
-                // set to unsafe previously, this will set it to safe
+
                 safety.ToggleSafety(rowIndex, colIndex);
             }
         }
@@ -38,25 +27,12 @@ public class Solution {
         return solvedCount;
     }
     
-    // Tracks which positions are attackable by another, current placed queen.
-    // All spaces are initialized to being safe (not attackable)
     private sealed class SafetyTable {
-        // The dimensions of the chess board
         private int dimensions;
-        
-        // The number of sequences of diagonally adjacent spaces
         private int diagCount;
-        
-        // Bit flags that track which rows are safe
         private int rowBitVector = -1;
-        
-        // Bit flags that track which columns are safe
         private int colBitVector = -1;
-        
-        // Bit flags that track which major diagonals are safe
         private int majorDiagBitVector = -1;
-        
-        // Bit flags that track which minor diagonals are safe
         private int minorDiagBitVector = -1;
         
         public SafetyTable(int dimensions) {
