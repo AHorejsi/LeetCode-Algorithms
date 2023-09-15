1eead3d757f84f7df337b4bbc0ec103fde2e09cb
Alex Horejsi <alex.horejsi59@gmail.com>
2023-09-14 18:11:03 -0700

    Update CarFleetGreedy.cs

--- leetcode/CarFleetGreedy.cs
+++ leetcode/CarFleetGreedy.cs
@@ -1,6 +1,6 @@
 public class Solution {    
     public int CarFleet(int target, int[] positions, int[] speeds) {
-        List<float> fleets =  Enumerable
+        List<float> fleets = Enumerable
             .Zip(positions, speeds)
             .OrderByDescending(((int position, int speed) data) => data.position) // Sort based on position so that fleets are adjacent to the fleets they would merge with first
             .Select(((int position, int speed) data) => (target - data.position) / (float)data.speed) // Calculate time it would take for each fleet to reach the end assuming no merges occur
