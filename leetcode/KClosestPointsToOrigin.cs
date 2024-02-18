71b255e42e77855eccc552e939b2b8fb90cfad5d
Alex Horejsi <alex.horejsi59@gmail.com>
2024-02-18 14:02:13 -0800

    Update KClosestPointsToOrigin.cs

--- leetcode/KClosestPointsToOrigin.cs
+++ leetcode/KClosestPointsToOrigin.cs
@@ -1,5 +1,4 @@
 public class Solution {
-    public int[][] KClosest(int[][] points, int k) => points.OrderBy(point => this.DistanceFromOrigin(point)).Take(k).ToArray();
-    
-    private double DistanceFromOrigin(int[] point) => Math.Sqrt(point[0] * point[0] + point[1] * point[1]);
+    public int[][] KClosest(int[][] points, int k) =>
+        points.OrderBy(point => Math.Sqrt(point[0] * point[0] + point[1] * point[1])).Take(k).ToArray();
 }
