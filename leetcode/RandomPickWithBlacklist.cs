033c6c0770cb6da06dc4a941e664816fbf911df6
Alex Horejsi <alex.horejsi59@gmail.com>
2024-02-18 12:31:57 -0800

    Create RandomPickWithBlacklist.cs

---
+++ leetcode/RandomPickWithBlacklist.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    private static readonly Random rand = new Random();
+    private readonly List<Range> ranges;
+
+    public Solution(int max, int[] blacklist) {
+        this.ranges = new List<Range>();
+
+        var iter = blacklist.OrderBy(number => number).GetEnumerator();
+        var low = 0;
+
+        while (iter.MoveNext()) {
+            var high = iter.Current;
+
+            this.AddRange(low, high);
+
+            low = high + 1;
+        }
+
+        this.AddRange(low, max);
+    }
+
+    private void AddRange(int low, int high) {
+        // If "low" and "high" are equal, then range is empty
+        if (low != high) {
+            this.ranges.Add(new Range(low, high));
+        }
+    }
+    
+    public int Pick() {
+        var index = Solution.rand.Next(0, this.ranges.Count);
+        var range = this.ranges[index];
+        
+        return Solution.rand.Next(range.Low, range.High);
+    }
+
+    private struct Range {
+        public int Low { get; init; }
+        public int High { get; init; }
+
+        public Range(int low, int high) {
+            this.Low = low;
+            this.High = high;
+        }
+    }
+}
